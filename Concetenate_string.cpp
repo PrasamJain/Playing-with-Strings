fbe6611305bc4ae4a961cf42a8d5e97176615feb
prasam jain <90105063+pj1807@users.noreply.github.com>
2022-05-05 22:38:23 +0530

    Update Concetenate_string.cpp

--- Concetenate_string.cpp
+++ Concetenate_string.cpp
@@ -1,3 +1,10 @@
+//The best way to concatenate two strings in C programming is by using the strcat() function.
+//However, in this example, we will concatenate two strings manually.
+//tt is the operation of joining two strings together.
+
+//Here, two strings s1 and s2 and concatenated and the result is stored in s1.
+//It's important to note that the length of s1 should be sufficient to hold the string after concatenation. If not, you may get unexpected output.
+
 #include<iostream>
 #include<string.h>
 #define N 30
@@ -25,3 +32,11 @@ int main()
 	cout<<"The string after concatenation is : "<<s1;
 	return 0;
 }
+
+
+/*   OUTPUT   */
+/*
+	Enter string s1: C++ Programming
+	Enter string s2:  is awesome.
+	Resultant String = C++ Programming is awesome.
+*/
