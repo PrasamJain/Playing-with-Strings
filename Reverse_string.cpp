afbe29f65a1124db627dbcbe15857692bc9e2058
prasam jain <90105063+pj1807@users.noreply.github.com>
2022-05-07 14:55:36 +0530

    Update Reverse_string.cpp

--- Reverse_string.cpp
+++ Reverse_string.cpp
@@ -1,3 +1,6 @@
+//One simple solution is to write our own reverse function to reverse a string in C++. [ without using reverse() inbuilt function ]
+// we reverse the string 's' and save in another string 'p'. 
+
 #include<iostream>
 #include<string.h>
 using namespace std;
@@ -12,4 +15,29 @@ int main()
 		p =p + s[i];
 	}
 	cout<<"Reversed string are : "<<p;
+	return 0;
+}
+
+/*__________________________________________________________________________________________________________________________________________________________________*/
+
+
+// The another method is using inbuilt “reverse” function:-
+// There is a direct function in “algorithm” header file for doing reverse that saves our time when programming.
+
+// Reverses elements in [begin, end]
+// Syntax : reverse ( BidirectionalIterator begin, BidirectionalIterator end )
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s;
+	cout<<"Enter the string : ";
+	getline(cin,s);
+	
+	// Reverse str[begin..end]
+	reverse(s.begin(), s.end());
+
+	cout<<"Reversed string are : "<<s;
+	return 0;
 }
